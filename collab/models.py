16dffe03bc0eb42c04260ff2ee985e5a81d51d7e
Kamil Aleksandrowicz <kamil.aleksandrowicz@infotech.edu.pl>
2023-10-20 12:08:45 +0200

    modele

--- collab/models.py
+++ collab/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Devices(models.Model):
+    devEui = models.CharField(max_length=100)
+    devName = models.CharField(max_length=100)
+    decodedPayload = models.JSONField(null=True)
+    time = models.DateTimeField(auto_now_add=True)
+    user = models.ManyToManyField('auth.User', related_name='devices', blank=True)
