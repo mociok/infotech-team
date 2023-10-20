0f385e7c4e54eb30c8305ef7fc30fca63789e5f5
Radek <radoslaw.laskowski@infotech.edu.pl>
2023-10-20 13:05:18 +0200

    Documentation of model Devices

--- collab/models.py
+++ collab/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Devices(models.Model):
-    devEui = models.CharField(max_length=100)
-    devName = models.CharField(max_length=100)
-    decodedPayload = models.JSONField(null=True)
-    time = models.DateTimeField(auto_now_add=True)
-    user = models.ManyToManyField('auth.User', related_name='devices', blank=True)
+    devEui = models.CharField(max_length=100) # primary key
+    devName = models.CharField(max_length=100) # unique Name of device
+    decodedPayload = models.JSONField(null=True) # decoded payload
+    time = models.DateTimeField(auto_now_add=True) # time of receiving data
+    user = models.ManyToManyField('auth.User', related_name='devices', blank=True) # user that have access to data from multiple devices
