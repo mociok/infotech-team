10714e6b9fa8a9cef94433e07c3bf67a7bd20610
Kamil Aleksandrowicz <kamil.aleksandrowicz@infotech.edu.pl>
2023-10-20 13:06:19 +0200

    def str do Devices model

--- collab/models.py
+++ collab/models.py
@@ -7,3 +7,6 @@ class Devices(models.Model):
     decodedPayload = models.JSONField(null=True)
     time = models.DateTimeField(auto_now_add=True)
     user = models.ManyToManyField('auth.User', related_name='devices', blank=True)
+
+    def __str__(self):
+        return f"{self.devName} - {self.devEui} - Users: {self.user.all().count()} // added at: {self.time.strftime('%m/%d/%Y, %H:%M')}"
