a456f306a935a92bd52b585cebb82a3268b2b384
Radek <radoslaw.laskowski@infotech.edu.pl>
2023-10-20 17:19:03 +0200

    Another comments

--- collab/urls.py
+++ collab/urls.py
@@ -4,6 +4,6 @@ from collab.views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('api/get_device_data/', LoraApi.as_view() , name='get_device_data'),
-    path('api/get_devices/', DevicesApi.as_view() , name='get_devices'),
+    path('api/get_device_data/', LoraApi.as_view() , name='get_device_data'), # get device data
+    path('api/get_devices/', DevicesApi.as_view() , name='get_devices'), # get devices
 ]
