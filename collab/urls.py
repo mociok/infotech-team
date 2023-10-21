8df132e33e9b79fcfbca44189c3977da140ce7ac
Kamil Aleksandrowicz <kamil.aleksandrowicz@infotech.edu.pl>
2023-10-21 17:30:27 +0200

    main to index a index to old

--- collab/urls.py
+++ collab/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from collab.views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('old/', index, name='index.old'),
     path('login/', login, name='login'),
-    path('main/', main, name='main'),
+    path('', main, name='index'),
     path('api/get_device_data/', LoraApi.as_view() , name='get_device_data'), # get device data
     path('api/get_devices/', DevicesApi.as_view() , name='get_devices'), # get devices
 ]
